11bad22d77a1c76a7a06ba0434181bfd345b8903
2jiyong <164735145+2jiyong@users.noreply.github.com>
2024-07-18 01:05:08 +0900

    Title: X 달리기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/X 달리기/x-run.py
+++ 240718/X 달리기/x-run.py
@@ -1,16 +1,30 @@
 x=int(input())
 
+def min_dis(n):
+    sum_num=0
+    for i in range(1,n+1):
+        sum_num+=i
+    return sum_num
+#n이 포함
+
 total=0
 v=1
 time=0
 while True:
-    total+=v
     time+=1
-    v+=1
-    if total*2==x:
-        ans=time*2
+    total+=v
+    if total+min_dis(v+1)<x:
+        v+=1
+    elif total+min_dis(v+1)==x:
+        ans=time*2+1
         break
-    if total*2>x:
-        ans=(time-1)*2+1
-        break
+    elif total+min_dis(v+1)>x:#유지혹은감소
+        if total+min_dis(v)<x:
+            continue
+            #유지
+        elif total+min_dis(v)==x:
+            ans=time+1
+            break
+        elif total+min_dis(v)>x:
+            v-=1
 print(ans)
