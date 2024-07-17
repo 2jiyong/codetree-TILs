69775c4358d4a3c8a21780b99a7dc8b27aaccf5f
2jiyong <164735145+2jiyong@users.noreply.github.com>
2024-07-17 23:33:03 +0900

    Title: 비둘기와 전기줄, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240717/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -3,10 +3,14 @@ arr=[list(map(int,input().split())) for _ in range(n)]
 pig=[-1]*(11)
 
 cnt=0
+
 for num,road in arr:
+
     if pig[num]==-1:
         pig[num]=road
     else:
         if pig[num]!=road:
             cnt+=1
+            pig[num]=road
+
 print(cnt)
